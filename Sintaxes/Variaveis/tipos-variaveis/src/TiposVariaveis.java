20ce43bddc121870895c8467378891e62e252dbf
wastenio rocha <wastenio.rocha@lynxprocess.com>
2024-04-30 11:05:35 -0300

    Tipos de variaveis.

--- Sintaxes/Variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ Sintaxes/Variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Wastenio";
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 10;
+        numero = 20;
+
+        System.out.println(numero);
+
+        // final nao deixa alterar o valor da variavel
+        final double VALOR_PI = 3.14;
     }
 }
