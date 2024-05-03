7038a06f77aa416e6c3e67a8feb94422f0c3af63
wastenio rocha <wastenio.rocha@lynxprocess.com>
2024-05-03 16:29:22 -0300

    Criando um sistema de funcionamento de TV

---
+++ Sintaxes/Exercitando/sistema_smartTv/src/Usuario.java
@@ -0,0 +1,28 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.trocarCanal(10);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+    }
+}
