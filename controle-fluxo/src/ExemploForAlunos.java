89f63cae55165fdb1a1d751f7e1bccee7234da11
wastenio rocha <wastenio.rocha@lynxprocess.com>
2024-06-11 20:39:19 -0300

    Estruturas de repetioções.

---
+++ controle-fluxo/src/ExemploForAlunos.java
@@ -0,0 +1,10 @@
+public class ExemploForAlunos {
+    public static void main(String[] args) {
+        String alunos[] = {"Alice", "Bob", "Charlie", "David", "Eve"};
+
+        for (int i = 0; i < alunos.length; i++){
+            System.out.println("Aluno no indice i=" + i + " é " + alunos[i]);
+        }
+    }
+    
+}
---
+++ controle-fluxo/src/ExemploForEach.java
@@ -0,0 +1,10 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos[] = {"Alice", "Bob", "Charlie", "David", "Eve"};
+
+        for (String aluno : alunos){
+            System.out.println("Aluno é " + aluno);
+        }
+    }
+    
+}
